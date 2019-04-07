b42c5e8c1a566f7c2a130bd02c2d74ed999b4323
Suzanne Smigielski <ssmigiel571@gmail.com>
2019-04-07 14:06:07 -0500

    updates to html & js

--- main.js
+++ main.js
@@ -1,33 +1,37 @@
 $(document).ready(function(){
 
-    $('#js-shopping-list-form').on('submit',() =>{
-        
-        let newItem = $('#shopping-list-entry').val();
-            
-             $('.shopping-list').append('<li>'+'<span class="shopping-item">'
-              + newItem + 
-             '</span>'+'<div class="shopping-item-controls">'+'
-             <button class="shopping-item-toggle">'+
-               '<span class="button-label">check</span>'+
-            '</button>'+
-             '<button class="shopping-item-delete">'+
-               '<span class="button-label">delete</span>'+
-             '</button>'+
-           '</div>'+'</li>');
-           $('#shopping-list-entry').val('').focus();
-           //$(event).preventDefault();
-      });
-        
+  $('#js-shopping-list-form').on('submit',(event) =>{
+
+      const newItem = $('#shopping-list-entry').val();
+      const html = 
+        <div class="shopping-item-controls">
+          <button class="shopping-item-toggle">
+            <span class="button-label">check</span>
+          </button>
+          <button class="shopping-item-delete">
+          <span class="button-label">delete</span>
+          </button>
+        </div>;
           
+           $('.shopping-list').append('<li>'+'<span class="shopping-item">'
+            + newItem + html + '</li>');
 
-    $('.shopping-item-toggle').on('click',() => {
-        $('.shopping-item').closest('li').toggleClass('.shopping-item_checked');
+         $('#shopping-list-entry').val('').focus();
+         $(event).preventDefault();
     });
+      
+        
 
-    $('.shopping-item-delete').click(()=>{
-        $(".shopping-item").closest('li').remove();
-    })
+  $('.shopping-item-toggle').on('click',(e) => {
+    const target= e.currentTarget; 
+      $('.shopping-item').parent('li').toggleClass('.shopping-item_checked');
+  });
 
-    
+  $('.shopping-item-delete').click((e)=>{
+    const target= e.currentTarget;
+      $(".shopping-item").parent('li').remove();
+  })
+
+  
 
 })
